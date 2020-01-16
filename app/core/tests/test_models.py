cf24635bf9ab6970764cc7773d34038313d67249
vanessaMora702 <vanessamora702@gmail.com>
2020-01-16 15:45:18 -0500

    PENDING SOLUTION

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,21 +1,22 @@
-# TEST CREATED USER
-
 from django.test import TestCase
-# IMPORT GET USER HELPER FUNCTION THAT COMES WITH DJANGO
 from django.contrib.auth import get_user_model
+# TEST CREATED USER
+# IMPORT GET USER HELPER FUNCTION THAT COMES WITH DJANGO
 
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """TEST CREATING A NEW USER WITH AN EMAIL IS SUCCESSFUL"""
+        """CREATTE THE EMAIL AND PASSWORD FOR A SECOND SO THAT THE TEST CAN RUN"""
         email = 'test@london.com'
         password = "Testpass123"
         user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
-
+        """ADD SOME ASSERTIONS SO TO KNOW WHERE THE EMAIL WAS CREATED CORRECTLY"""
         self.assertEqual(user.email, email)
+        """PASSWORD IS ENCRYPTED SO YOU HAVE TO USE THE CHECK PASSWORD FUNCTION"""
         self.assertTrue(user.check_password, password)
 
     def test_new_user_email_normalized(self):
